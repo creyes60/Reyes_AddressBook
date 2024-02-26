cbb9273c69c7f2a85b64370e6b9b3a251f1880d8
creyes60 <142647746+creyes60@users.noreply.github.com>
2024-02-25 21:15:35 -0800

    added documentation

--- Menu.java
+++ Menu.java
@@ -1,14 +1,29 @@
 import java.util.Scanner;
-
+/**
+ * The Menu class represents a simple menu for an address book application.
+ * It provides options for loading entries from a file, adding, removing, finding, and listing entries, as well as quitting the program.
+ *
+ * @author Christian Reyes
+ * @version 1.0
+ * @since 2024-02-10
+ */
 public class Menu {
     private Scanner scanner;
 
     // Constructor
+    /**
+     * Constructs a Menu object with a new Scanner.
+     */
     public Menu() {
         this.scanner = new Scanner(System.in);
     }
 
     // Display the menu and get user choice
+    /**
+     * Displays the menu options and prompts the user to enter their choice.
+     *
+     * @return The user's choice as a character.
+     */
     public char displayMenu() {
         System.out.println("a) Loading of entries from a file.");
         System.out.println("b) Addition - New Address Entry");
